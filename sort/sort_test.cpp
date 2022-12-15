652f6f3653343da24529731f83803f1aef5942a1
MaoSong2022 <maosong@pjlab.org.cn>
2022-12-15 22:37:37 +0800

    feat(sort_test.hpp):use test fixtures to rewrite tests

--- sort/sort_test.cpp
+++ sort/sort_test.cpp
@@ -9,40 +9,104 @@
 #include "merge_sort.hpp"
 #include "quick_sort.hpp"
 #include "heap_sort.hpp"
+#include "selection_sort.hpp"
+#include "bubble_sort.hpp"
 
-TEST(SORT, QuickSort){
-    std::vector<int> array{5, 4, 3, 8, 6, 2, 1, 9, 7, 0};
-    std::vector<int> result{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Sort::quick_sort(array);
 
-    ASSERT_EQ(result.size(), array.size()) << "Vectors result and array are of unequal length";
+class SortTest : public ::testing::Test {
+public:
+    std::vector<int> original_array;
+    std::vector<int> reuse_array;
+    std::vector<int> array_increase;
+    std::vector<int> array_decrease;
 
-    for (int i = 0; i < result.size(); ++i) {
-        EXPECT_EQ(result[i], array[i]) << "Vectors result and array differ at index " << i;
+protected:
+    void SetUp() override {
+        original_array = {5, 4, 3, 8, 6, 2, 1, 9, 7, 0};
+        reuse_array = original_array;
+        array_increase = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        array_decrease = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     }
 };
 
-TEST(SORT, HeapSort){
-    std::vector<int> array{5, 4, 3, 8, 6, 2, 1, 9, 7, 0};
-    std::vector<int> result{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Sort::heap_sort(array);
 
-    ASSERT_EQ(result.size(), array.size()) << "Vectors result and array are of unequal length";
-
-    for (int i = 0; i < result.size(); ++i) {
-        EXPECT_EQ(result[i], array[i]) << "Vectors result and array differ at index " << i;
+TEST_F(SortTest, QuickSort) {
+    Sort::quick_sort(reuse_array);
+    ASSERT_EQ(array_increase.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_increase.size(); ++i) {
+        EXPECT_EQ(array_increase[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
     }
+    reuse_array = original_array;
+
+    Sort::quick_sort<int, std::greater<int>>(reuse_array);
+    ASSERT_EQ(array_decrease.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_decrease.size(); ++i) {
+        EXPECT_EQ(array_decrease[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
 };
 
-TEST(SORT, MergeSort){
-    std::vector<int> array{5, 4, 3, 8, 6, 2, 1, 9, 7, 0};
-    std::vector<int> result{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Sort::merge_sort(array);
-
-    ASSERT_EQ(result.size(), array.size()) << "Vectors result and array are of unequal length";
-
-    for (int i = 0; i < result.size(); ++i) {
-        EXPECT_EQ(result[i], array[i]) << "Vectors result and array differ at index " << i;
+TEST_F(SortTest, HeapSort) {
+    Sort::heap_sort(reuse_array);
+    ASSERT_EQ(array_increase.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_increase.size(); ++i) {
+        EXPECT_EQ(array_increase[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
     }
+    reuse_array = original_array;
+
+    Sort::heap_sort<int, std::greater<int>>(reuse_array);
+    ASSERT_EQ(array_decrease.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_decrease.size(); ++i) {
+        EXPECT_EQ(array_decrease[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+};
+
+TEST_F(SortTest, MergeSort) {
+    Sort::merge_sort(reuse_array);
+    ASSERT_EQ(array_increase.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_increase.size(); ++i) {
+        EXPECT_EQ(array_increase[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+
+    Sort::merge_sort<int, std::greater<int>>(reuse_array);
+    ASSERT_EQ(array_decrease.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_decrease.size(); ++i) {
+        EXPECT_EQ(array_decrease[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+};
+
+TEST_F(SortTest, SelectionSort) {
+    Sort::selection_sort(reuse_array);
+    ASSERT_EQ(array_increase.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_increase.size(); ++i) {
+        EXPECT_EQ(array_increase[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+
+    Sort::selection_sort<int, std::greater<int>>(reuse_array);
+    ASSERT_EQ(array_decrease.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_decrease.size(); ++i) {
+        EXPECT_EQ(array_decrease[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+};
+
+TEST_F(SortTest, BubbleSort) {
+    Sort::bubble_sort(reuse_array);
+    ASSERT_EQ(array_increase.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_increase.size(); ++i) {
+        EXPECT_EQ(array_increase[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
+
+    Sort::bubble_sort<int, std::greater<int>>(reuse_array);
+    ASSERT_EQ(array_decrease.size(), reuse_array.size()) << "Vectors result and array are of unequal length";
+    for (int i = 0; i < array_decrease.size(); ++i) {
+        EXPECT_EQ(array_decrease[i], reuse_array[i]) << "Vectors result and array differ at index " << i;
+    }
+    reuse_array = original_array;
 };
 
